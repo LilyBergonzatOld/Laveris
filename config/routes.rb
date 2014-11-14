d9baada0e661d17e39d24a9ba273e4e0ad45ea33
Yann Bergonzat <yannbergonzat@gmail.com>
2014-11-14 18:13:09 +0100

    Scaffold done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   root to: 'users#index', via: :get
   get 'auth/facebook', as: "auth_provider"
   get 'auth/facebook/callback', to: 'users#login'
