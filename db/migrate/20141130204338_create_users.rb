fa991084317f1671a0fe4c5f5d12e790d1c72966
Yann <yannbergonzat@gmail.com>
2014-12-04 23:38:42 +0100

    Made id column bigger for users

--- db/migrate/20141130204338_create_users.rb
+++ db/migrate/20141130204338_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :users do |t|
       t.integer :id, :limit => 8
-      
+
       t.string :name
       t.string :surname
       t.string :phone
